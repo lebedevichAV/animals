876ba46697dd81d19c2c39347f4a3ce991a939ed
lebedevichAV <lebedevichav@gmail.com>
2022-10-27 11:33:07 +0300

    feat: add slider module, add slide mixin

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+import Swiper from "swiper";
+import "swiper/css";
+import "swiper/css/navigation"
 import "@/styles";
 
+const swiper = new Swiper(".slider", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
