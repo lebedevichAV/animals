51cfef0674f7c634794e83433825be4f2758b3a8
lebedevichAV <lebedevichav@gmail.com>
2022-10-27 17:10:52 +0300

    refactor: delete swiper modeule with styles for it

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-import Swiper, { Navigation } from "swiper";
-import "swiper/css";
-import "swiper/css/navigation";
 import "@/styles";
-
-const swiper = new Swiper(".swiper", {
-  modules: [Navigation],
-  slidesPerView: 1,
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
