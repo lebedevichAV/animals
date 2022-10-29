3d5c8661274a829f1753ae8d2a9f1472e8041623
lebedevichAV <lebedevichav@gmail.com>
2022-10-29 15:42:06 +0300

    feat: create new mixin - card, add new files for scale card images, add listenets for cards images;

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ import "@/styles";
 $(document).ready(function () {
   $(".slider").slick();
 });
+
+let cardPhotos = document.querySelectorAll(".card__photo");
+
+for (let cardPhoto of cardPhotos) {
+  cardPhoto.addEventListener("mouseover", function () {
+    cardPhoto.classList.toggle("card__photo_chosen");
+  });
+  cardPhoto.addEventListener("mouseout", function () {
+    cardPhoto.classList.toggle("card__photo_chosen");
+  });
+}
